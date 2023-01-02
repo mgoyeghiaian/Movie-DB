bebf99e2ebe709ffe6547f4798903eb4a9fd7405
mgoyeghiaian <mgoyeghiaian@gmail.com>
2023-01-02 02:49:23 +0200

    Step 4

--- index.js
+++ index.js
@@ -14,6 +14,23 @@ app.get("/time", (req, res) => {
   });
 });
 
+app.get(`/hello/:id`, (req, res) => {
+  const id = req.params.id;
+  res.json({ status: 200, message: `Hello, ${id}` })
+});
+
+app.get(`/search`, (req, res) => {
+  const search = req.query.s;
+  if (search) {
+    res.json({ status: 200, message: "ok", data: search })
+  }
+
+  else {
+    res.status(500).json({status:500, error:true, message: "you have to provide a search" })
+  }
+
+});
+
 app.listen(port, () => {
   console.log(`Server Listening on port ${port}`)
 });
